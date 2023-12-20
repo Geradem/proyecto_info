55fe928c75f288ed435766f256c7fb53a1bff7aa
Geradem <emilianogerzel@gmail.com>
2023-12-20 11:19:37 -0300

    Fin clase 30 de noviembre de 2023

--- blog/apps/posts/views.py
+++ blog/apps/posts/views.py
@@ -1,7 +1,7 @@
 #import get_object
 from django.shortcuts import render, get_object_or_404
 #importar la clase Post del Models(BD)
-from .models import Post
+from .models import Post, Categoria
 
 
 
@@ -14,11 +14,22 @@ def post(request):
     return render(request, 'post.html')
 
 def post_realizado(request):
-    posteos = Post.objects.all() # Selec * from post
-    return render(request, 'posts/post.html', {'posteos':posteos})
+    # posteos = Post.objects.all() # Selec * from post
+    # categorias = Post.objects.all()
+    id_categoria = request.GET.get('id', None)
+    if id_categoria:
+        posteos = Post.objects.filter(categoria_post = id_categoria)
+    else:
+        posteos = Post.objects.all() #una lista
+        
+    categorias = Categoria.objects.all()
+    ctx = zip(posteos, categorias)
+
+    return render(request, 'posts/post.html', {'ctx': ctx, 'posteos':posteos})
 
 def post_detail(request, post_id):
     #busca el post y si no encuentra err 404
     post =get_object_or_404(Post, pk=post_id)
     ctx = {"post": post}
     return render(request, 'posts/post_detail.html', ctx )
+
